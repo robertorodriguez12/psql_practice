7cf708101c1c8cb4dd31e12faa91611c8fc0c351
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-11-23 13:02:35 -0700

    Initial

---
+++ express.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Publications';
+
+app.get('/api/v1/papers/:id', (request, response) => {
+    database('papers').where('id', request.params.id).select()
+        .then(papers => {
+            if (papers.length) {
+                response.status(200).json(papers);
+            } else {
+                response.status(404).json({
+                    error: `Could not find paper with id ${request.params.id}`
+                });
+            }
+        })
+        .catch(error => {
+            response.status(500).json({ error });
+        });
+});
+
+app.post('/api/v1/papers', (request, response) => {
+    const paper = request.body;
+
+    for (let requiredParameter of ['title', 'author']) {
+        if (!paper[requiredParameter]) {
+            return response
+                .status(422)
+                .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.` });
+        }
+    }
+
+    database('papers').insert(paper, 'id')
+        .then(paper => {
+            response.status(201).json({ id: paper[0] })
+        })
+        .catch(error => {
+            response.status(500).json({ error });
+        });
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
